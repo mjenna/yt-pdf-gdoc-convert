85c5735c21ce8175a72520c8ac48e75149862a0c
Jenna Moon <moon0jenna@gmail.com>
2025-03-21 16:16:12 -0400

    takes in multiple links

--- yt.py
+++ yt.py
@@ -1,10 +1,17 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import sys
 
-video_url = input("Paste URL of the YouTube video: ").strip()
+
+#handling multiple URLS
+video_urls = input("Paste URLs of the YT video (separated by commas if multiple): ").strip().split(",")
+
+#video_url = input("Paste URL of the YouTube video: ").strip()
 
 #extracts video id
-video_id = video_url.split("v=")[1].split("&")[0]
+#video_id = video_url.split("v=")[1].split("&")[0]
+for video_url in video_urls:
+    video_url = video_url.strip() #remove any white space
+    video_id = video_url.split("v=")[1].split("&")[0] #extract video id
 
 try:
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
@@ -12,9 +19,12 @@ except Exception as e:
     print(f"Cannot get the transcript: {e}")
     sys.exit(1)
 
-#print transcript into a text file
+#print transcripts into text files
+transcript_filename = f"transcript_{video_id}.txt"
 with open("transcript.txt", "w") as file:
     for entry in transcript:
         file.write(f"{entry['start']}: {entry['text']}\n")
 
-print("transcript has been inserted into transcript.txt file")
+print("transcript for [video url] has been inserted into [transcript_filename].txt file")
+
+
